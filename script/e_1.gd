9ecac77adad13fb2f97df2328abaea1663e741fd
fuadhasan24000 <fuadhasan24000@gmail.com>
2026-09-11 07:41:32 +0600

    added player and an enemy

---
+++ script/e_1.gd
@@ -0,0 +1,46 @@
+extends Area2D
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
+var health = 60
+# Called when the node enters the scene tree for the first time.
+var isdamaged = false
+var isdead = false
+
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+	
+
+	
+
+
+
+	
+		
+
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+	if animated_sprite_2d.animation == "damage":
+		isdamaged= false
+	if animated_sprite_2d.animation == "death":
+		queue_free()
+		
+
+
+func _on_area_entered(area: Area2D) -> void:
+	
+	if area.is_in_group("melee1"):
+		print(health)
+		health= health - 20
+		isdamaged =true
+		
+		if health <= 0:
+			isdead= true
+			animated_sprite_2d.play("death")
+		if not isdead:
+			animated_sprite_2d.play("damage")
+			
--- script/player.gd
+++ script/player.gd
@@ -1,25 +1,67 @@
 extends CharacterBody2D
 
+@onready var animation: AnimatedSprite2D = $animation
 
-const SPEED = 300.0
+
+var health = 100
+const melee_damage = 20 
+var isattaking = false;
+var SPEED =200
 const JUMP_VELOCITY = -400.0
 
 
+
+
+	
+
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
+
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump ") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+	var direction := Input.get_axis("left", "right")
+	if direction> 0:
+		animation.flip_h = false
+	elif direction < 0:
+		animation.flip_h = true
+	if Input.is_action_just_pressed("attack melee"):
+		animation.play("attack(melee)")
+		isattaking = true 
+		$Area2D/CollisionShape2D.disabled = false
+	if is_on_floor() and isattaking ==false :
+		
+		if direction !=0 and Input.is_action_pressed("run"):
+			animation.play("run")
+		elif direction !=0 :
+			animation.play("walk")
+		else:
+			animation.play("idle")
+	elif isattaking ==false:
+		animation.play("jump")
+	
+
+	if direction and Input.is_action_pressed("run"):
+		velocity.x = direction * SPEED*2 
+	elif direction:
+		velocity.x = direction * SPEED 
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+
+
+
+
+
+func _on_animation_animation_finished() -> void:
+	if animation.animation == "attack(melee)":
+		isattaking = false
+		$Area2D/CollisionShape2D.disabled = true 
